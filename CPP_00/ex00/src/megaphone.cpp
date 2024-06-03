29e8b3f24b26a56d5bfc8970997bc237030888b1
antonweizmann <anton@weizmann.de>
2024-06-03 17:14:08 +0200

    cpp_00 starded ex00 done

---
+++ CPP_00/ex00/src/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int	main(int argc, char **argv){
+	int	i;
+
+	for (i = 1; i < argc; i++)
+	{
+		std::string str = argv[i];
+		std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+		if (i == argc - 1) std::cout << str << std::endl;
+		else std::cout << str;
+	}
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
