97f39f58ff9ddd8a007f369767b880835859ea9a
antonweizmann <anton@weizmann.de>
2024-06-24 16:57:46 +0200

    starting ex01

---
+++ CPP_04/ex01/src/Cat.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweizman <aweizman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 15:48:53 by aweizman          #+#    #+#             */
+/*   Updated: 2024/06/24 15:37:40 by aweizman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat() : Animal()
+{
+	type = "Cat";
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat destructer called" << std::endl;
+}
+
+Cat::Cat(const Cat& src)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	*this = src;
+}
+
+Cat& Cat::operator=(const Cat& src)
+{
+	std::cout << "Cat copy assignment operator called" << std::endl;
+	if (this != &src)
+	{
+		type = src.type;
+	}
+	return (*this);
+}
+
+void	Cat::makeSound() const
+{
+	std::cout << "Cat sound: Miao Miao" << std::endl;
+}
